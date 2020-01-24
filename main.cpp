e8ce6cfef18002121b19915d9dcf6d05d8f3d1ea
Emanuel Sprung <emanuel.sprung@gmail.com>
2020-01-25 00:05:07 +0100

    Meta: More work on command line options

--- main.cpp
+++ main.cpp
@@ -1,36 +1,80 @@
-#include <stdio.h>
+#include <AK/Types.h>
+#include <algorithm>
 #include <cstring>
+#include <stdio.h>
+
+enum class PrimaryCommand : u8 {
+    None = 0,
+    Build,
+    Config,
+    Run
+};
+
+enum class ConfigSubCommand : u8 {
+    None = 0,
+    Get,
+    Set,
+    List
+};
 
 int main(int argc, char** argv)
 {
     int minarg = 2;
+    PrimaryCommand cmd = PrimaryCommand::None;
+    ConfigSubCommand config_subcmd = ConfigSubCommand::None;
 
     if (argc >= minarg) {
-        if(strncmp(argv[1], "build", 5) == 0) {
+        int arg_len = strlen(argv[1]);
+
+        if (strncmp("build", argv[1], arg_len) == 0 && arg_len == 5) {
             minarg = 3;
-        }
-        else if(strncmp(argv[1], "config", 6)) {
-            if(strncmp(argv[2], "set", 3) == 0)
-                minarg = 5;
-            else if(strncmp(argv[2], "get", 3) == 0)
-                minarg = 4;
-            else
-                minarg = 3;
+            cmd = PrimaryCommand::Build;
+        } else if (strncmp("config", argv[1], arg_len) == 0 && arg_len == 6) {
+            cmd = PrimaryCommand::Config;
+            ++minarg;
+            if (argc >= minarg) {
+                int subarg_len = strlen(argv[2]);
+                if (strncmp(argv[2], "set", subarg_len) == 0 && subarg_len == 3) {
+                    minarg = 5;
+                    config_subcmd = ConfigSubCommand::Set;
+                } else if (strncmp(argv[2], "get", subarg_len) == 0 && subarg_len == 3) {
+                    minarg = 4;
+                    config_subcmd = ConfigSubCommand::Get;
+                } else if (strncmp(argv[2], "list", subarg_len) == 0 && subarg_len == 4) {
+                    minarg = 3;
+                    config_subcmd = ConfigSubCommand::List;
+                } else {
+                    minarg = -1;
+                }
+            }
+        } else if (strncmp(argv[1], "run", arg_len) == 0 && arg_len == 3) {
+            cmd = PrimaryCommand::Run;
+            minarg = 2;
         }
     }
 
-    if (argc != minarg) {
+    if (argc < minarg || minarg < 0 || cmd == PrimaryCommand::None) {
         fprintf(stderr, "usage: \n");
-        fprintf(stderr, "  Config:\n");
+        if (cmd == PrimaryCommand::None || (cmd == PrimaryCommand::Config && config_subcmd == ConfigSubCommand::None)) {
+            fprintf(stderr, "  Config:\n");
         fprintf(stderr, "    meta config list\n");
         fprintf(stderr, "    meta config get <param>\n");
         fprintf(stderr, "    meta config set <param> <value>\n");
-        fprintf(stderr, "  Build:\n");
+        }
+        if (cmd == PrimaryCommand::None || cmd == PrimaryCommand::Build) {
+            fprintf(stderr, "  Build:\n");
         fprintf(stderr, "    meta build <toolchain>\n");
         fprintf(stderr, "    meta build <application>\n");
         fprintf(stderr, "    meta build <image>\n");
+        }
+        if (cmd == PrimaryCommand::None || cmd == PrimaryCommand::Run) {
+            fprintf(stderr, "  Run:\n");
+            fprintf(stderr, "    meta run [<image>]\n");
+        }
         return 0;
     }
+
+    (void)(config_subcmd);
 
     return 0;
 }
